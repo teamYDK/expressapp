d2e8dd762fafa543ee565a6e4708146ca7682a39
戸谷咲良 <v1016.pickles@gmail.com>
2017-08-24 13:07:27 +0900

    データベースにデータ保存までできた

--- file-upload/routes/index.js
+++ file-upload/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var firebase=require('firebase');
-var upload = multer({ dest: './uploads/' }).single('thumbnail');
+var upload = multer({ dest: './uploads/' }).single('picture');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 router.post('/upload', function(req, res) {/*データあげてます*/
   upload(req, res, function(err) {//非同期の処理　upload　が読まれて次にいく、upload終わったらfunction実行される
     if(err) {//エラーの時
-      res.send("Failed to write " + req.file.destination + " with " + err);
+      res.send("Failed to write " + req.picture.destination + " with " + err);
     } else {//成功の時
       firebase.initializeApp({
         databaseURL:"https://ydk-6f9ed.firebaseio.com"
@@ -20,8 +20,10 @@ router.post('/upload', function(req, res) {/*データあげてます*/
       var firebaseRef =firebase.database().ref();//追加
       var messagesRef = firebaseRef.child('messages');//追加
       messagesRef.push({ // ...　囲んでる部分の描き方は変わらない 非同期処理
-        name: req.body.name,//かわる
-        body: req.body.body
+        username: req.body.username,
+        title:    req.body.title,
+        comment:  req.body.comment,
+        file:     req.file.filename
       }).then(function(){
         res.send("uploaded " + req.file.originalname + " as " + req.file.filename + " Size: " + req.file.size);//画面の表示
       });
