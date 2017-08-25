3df42ca0d2de56292dd013d4a7f75912aef472b2
戸谷咲良 <v1016.pickles@gmail.com>
2017-08-25 19:58:36 +0900

    緯度経度を抽出できるようにした

--- file-upload/routes/index.js
+++ file-upload/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var multer = require('multer');
 var firebase=require('firebase');
 var upload = multer({ dest: './uploads/' }).single('picture');
+var ExifImage = require('exif').ExifImage;
 
 firebase.initializeApp({
   databaseURL:"https://ydk-6f9ed.firebaseio.com"
@@ -24,14 +25,41 @@ router.get('/', function(req, res, next) {
 router.post('/upload', function(req, res) {/*データあげてます*/
   upload(req, res, function(err) {//非同期の処理　upload　が読まれて次にいく、upload終わったらfunction実行される
     if(err) {//エラーの時
-      res.send("Failed to write " + req.picture.destination + " with " + err);
+      res.send("Failed to write " + req.file.destination + " with " + err);
     } else {//成功の時
+
+/*------------------------------度数秒形式から変換---------------------------*/
+
+      filepath = req.file.destination + req.file.filename;
+
+      var convert = function(dms){
+        // var dms = [ 35, 40, 47.25 ];
+        return dms[0] + ( dms[1] * 60 + dms[2] ) / 3600
+      }
+
+      var lat,lon = 0;
+
+      new ExifImage({ image : filepath }, function (error, image) {
+        if (error) {
+          console.log('Error: '+error.message);
+        } else {
+          lat = convert((image['gps']['GPSLatitude']));
+          lon = convert((image['gps']['GPSLongitude']));
+        }
+      });
+
+/*------------------------------度数秒形式から変換---------------------------*/
+
+      console.log(lat, lon);
+
       var firebaseRef =firebase.database().ref();//追加
       var messagesRef = firebaseRef.child('messages');// データベースの参照の取得
       messagesRef.push({ // ...　囲んでる部分の描き方は変わらない 非同期処理
         username: req.body.username,
         title:    req.body.title,
         comment:  req.body.comment,
+        // lat:      lat,//リクエストした値でない
+        // lon:      lon,//リクエストした値でない
         file:     req.file.filename
       }).then(function(){
         res.send("uploaded " + req.file.originalname + " as " + req.file.filename + " Size: " + req.file.size);//画面の表示
