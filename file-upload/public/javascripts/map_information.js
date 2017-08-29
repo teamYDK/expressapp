504fb3abe58e6c4bf95867c197c63ba237c4279c
juko <sakikaorucha027@gmail.com>
2017-08-29 09:40:54 +0900

    表示エラー直しました

--- file-upload/public/javascripts/map_information.js
+++ file-upload/public/javascripts/map_information.js
@@ -233,6 +233,8 @@ function initMap(){
 
     var uluru = {lat: -25.363, lng: 131.044};
 
+    var hana = {lat: 35.719804, lng: 139.734385}
+
     //var contentString =
 
     /*'<ul>'+
@@ -268,7 +270,7 @@ function initMap(){
 
 
 
-    var infowindow = new google.maps.InfoWindow({
+  var infowindow = new google.maps.InfoWindow({
 
       /*<ul>
         <% for item in @messages : %>
@@ -324,7 +326,7 @@ function initMap(){
 
 
     var marker = new google.maps.Marker({
-      position: uluru,
+      position: hana,
       map: map,
 
 	/*  title: 'Uluru (Ayers Rock)' */
@@ -336,62 +338,3 @@ function initMap(){
 
 
 }
-
-$(function(){
-
-    //モーダルウィンドウを出現させるクリックイベント
-    $("#modal-open").click( function(){
-
-	//キーボード操作などにより、オーバーレイが多重起動するのを防止する
-	$( this ).blur() ;	//ボタンからフォーカスを外す
-	if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
-	//if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
-
-	//オーバーレイを出現させる
-	$( "body" ).append( '<div id="modal-overlay"></div>' ) ;
-	$( "#modal-overlay" ).fadeIn( "slow" ) ;
-
-	//コンテンツをセンタリングする
-	centeringModalSyncer() ;
-
-	//コンテンツをフェードインする
-	$( "#modal-content" ).fadeIn( "slow" ) ;
-
-	//[#modal-overlay]、または[#modal-close]をクリックしたら…
-	$( "#modal-overlay,#modal-close" ).unbind().click( function(){
-
-	    //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
-	    $( "#modal-content,#modal-overlay" ).fadeOut( "slow" , function(){
-
-		//[#modal-overlay]を削除する
-		$('#modal-overlay').remove() ;
-
-	    } ) ;
-
-	} ) ;
-
-    } ) ;
-
-    //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
-    $( window ).resize( centeringModalSyncer ) ;
-
-    //センタリングを実行する関数
-    function centeringModalSyncer() {
-
-	//画面(ウィンドウ)の幅、高さを取得
-	var w = $( window ).width() ;
-	var h = $( window ).height() ;
-
-	// コンテンツ(#modal-content)の幅、高さを取得
-	// jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
-	//		var cw = $( "#modal-content" ).outerWidth( {margin:true} );
-	//		var ch = $( "#modal-content" ).outerHeight( {margin:true} );
-	var cw = $( "#modal-content" ).outerWidth();
-	var ch = $( "#modal-content" ).outerHeight();
-
-	//センタリングを実行する
-	$( "#modal-content" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
-
-    }
-
-} ) ;
